5d864c042934b5a71759c34c6ae386387a98fba8
VinnyL12 <71726155+VinnyL12@users.noreply.github.com>
2021-12-13 20:24:04 -0500

    Updated Interface Example to fix Minimum output

--- InterfaceExample/src/MeasurableTester.java
+++ InterfaceExample/src/MeasurableTester.java
@@ -1,11 +1,24 @@
 /**
    This program demonstrates the measurable Country class.
 */
+
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.util.Scanner;
+
 public class MeasurableTester
 {
+	
+	private static final int FRAME_WIDTH = 200;
+	private static final int FRAME_HEIGHT = 400;
+	
    public static void main(String[] args)
    {
      
+	   Scanner in = new Scanner(System.in);
+	   
 	  //Countries
 	  System.out.println("Countries Data");
 	  Measurable[] countries = new Measurable[3];
@@ -19,22 +32,30 @@ public class MeasurableTester
       double maxData = Data.max(countries);
       System.out.println("Maximum area: " + maxData);
       System.out.println("Expected: 513120");
-      System.out.println("");
+      System.out.println();
+      double minData = Data.min(countries);
+      System.out.println("Minimum area: " + minData);
+      System.out.println("Expected: ");
+      System.out.println();
      
       //Bank Accounts
       System.out.println("Bank account data");
-      Measurable[] bank = new Measurable[3];
-      bank[0] = new BankAccount(176220);
-      bank[1] = new BankAccount(513120);
-      bank[2] = new BankAccount(30510);
-      avg=Data.average(bank);
+      Measurable[] accounts = new Measurable[3];
+      accounts[0] = new BankAccount(176220);
+      accounts[1] = new BankAccount(513120);
+      accounts[2] = new BankAccount(30510);
+      avg=Data.average(accounts);
       System.out.println("Average balance: " + avg);
       System.out.println("Expected: 239950");
       System.out.println("");
-      maxData = Data.max(bank);
+      maxData = Data.max(accounts);
       System.out.println("Maximum balance: " + maxData);
       System.out.println("Expected: 513120");
       System.out.println("");
+      minData = Data.min(accounts);
+      System.out.println("Minimum balance: " + minData);
+      System.out.println("Expected: ");
+      System.out.println();
 
       //Quiz
       System.out.println("Quiz data");
@@ -51,6 +72,51 @@ public class MeasurableTester
       maxData = Data.max(quiz);
       System.out.println("Highest Score: " + maxData);
       System.out.println("Expected: 95.0");
+      System.out.println();
+      minData = Data.min(accounts);
+      System.out.println("Minimum balance: " + minData);
+      System.out.println("Expected: ");
+      System.out.println();
 
+      
+      System.out.println("Enter metric to use, 1 for maximum, 2 for minimum, 3 for average");
+      int whichMetric = in.nextInt();
+      String metricToUse = "average";
+      if(whichMetric == 1)
+      {
+    	  metricToUse = "Maximum";
+      }
+      if(whichMetric == 2)
+      {
+    	  metricToUse = "Minimum";
+      }
+      if(whichMetric == 3)
+      {
+    	  metricToUse = "Average";
+      }
+      
+      JPanel panel = new JPanel();
+      JFrame frame = new JFrame();
+      frame.add(panel);
+
+
+      JButton button = new JButton("Get " + metricToUse + " Balance");
+      panel.add(button);
+      ActionListener listener = new ButtonListener(accounts, metricToUse);
+      button.addActionListener(listener);
+      
+      JButton button2 = new JButton("Get " + metricToUse + " Area");
+      panel.add(button2);
+      ActionListener listener2 = new ButtonListener(countries, metricToUse);
+      button2.addActionListener(listener2);
+      
+      JButton button3 = new JButton("Get " + metricToUse + " Score");
+      panel.add(button3);
+      ActionListener listener3 = new ButtonListener(quiz, metricToUse);
+      button3.addActionListener(listener3);
+
+      frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
    }
 }
